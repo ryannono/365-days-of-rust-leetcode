55ded8f49d51a8d4fb138e46bf79794e6dc89099
Ryan Nono <ryan_nono@hotmail.com>
2023-09-21 19:38:18 -0400

    improved list manipulation

--- src/delete_duplicates.rs
+++ src/delete_duplicates.rs
@@ -1,8 +1,45 @@
 // Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug, Default)]
 pub struct ListNode {
 	pub val: i32,
 	pub next: Option<Box<ListNode>>,
+}
+
+/// This trait provides the ability for a node to delete itself from a list.
+trait DeleteNode<T> {
+	fn delete(&mut self) -> Option<&mut T>;
+}
+
+impl DeleteNode<Self> for Box<ListNode> {
+	/// Deletes the current node (`self`) and replaces it with the next node
+	/// (`self.next`), effectively skipping over one node in the list.
+	///
+	/// This function performs an in-place modification of the list.
+	///
+	/// ## Workflow:
+	/// 1. Check if there's a next node (`self.next.is_some()`).
+	/// 2. If there's a next node, we use the `Option::take` method to
+	///    temporarily detach it from the list.
+	/// 3. We then overwrite the current node (`*self`) with its next node.
+	///    This effectively "deletes" the current node by overwriting
+	///    it with its successor.
+	/// 4. Return a mutable reference to the modified current node.
+	///
+	/// If there's no next node (i.e., `self` is the last node), this function
+	/// simply returns a mutable reference to the `None` value succeeding the last node.
+	///
+	/// # Returns
+	///
+	/// * `Option<&mut Self>` - A mutable reference to the next node after
+	///   deletion or the tail of the list if there are no more nodes.
+	fn delete(&mut self) -> Option<&mut Self> {
+		if self.next.is_some() {
+			*self = self.next.take()?; // Overwrite the current node with the next node.
+			return Some(self); // Return the modified current node.
+		}
+
+		self.next.as_mut()
+	}
 }
 
 /**
@@ -28,16 +65,14 @@ Removes duplicate elements from a sorted linked list.
 */
 pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
 	let mut head = head;
-	let mut ptr: &mut Option<Box<ListNode>> = &mut head;
+	let mut ptr = head.as_mut();
 
 	while let Some(node) = ptr {
-		if node.next.as_ref().is_some_and(|next| next.val == node.val) {
-			node.next = node.next.take().and_then(|next| next.next);
-		} else {
-			ptr = match ptr {
-				Some(x) => &mut x.next,
-				None => ptr,
-			}
+		ptr = match node.next.as_ref().is_some_and(|next| next.val == node.val)
+		{
+			true => node.delete(),
+
+			false => node.next.as_mut(),
 		}
 	}
 
